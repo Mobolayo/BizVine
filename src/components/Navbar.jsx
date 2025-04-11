ad1eaf358c7920e6092c9cf77266f202b90ce605
Fatimah Badmos <82815523+Fatimatulzahra@users.noreply.github.com>
2025-04-11 11:14:33 -0500

    saving a change Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,3 +38,48 @@ export default function Navbar() {
     </nav>
   );
 }
+
+
+//  <nav className="sticky top-0 z-50 w-full bg-gradient-to-r from-pink-200 via-pink-300 to-pink-400 shadow-lg">
+//       <div className="mx-auto flex h-[60px] items-center justify-between max-w-screen-xl px-6">
+//         {/* Logo */}
+//         <div className="flex items-center text-darkred font-bold text-2xl tracking-wide gap-2 transform transition-transform duration-300 hover:scale-105 hover:-translate-y-1">
+//           <Briefcase className="w-6 h-6 text-darkred" />
+//           <span>BizVine</span>
+//         </div>
+// .dashboard-container {
+//   padding: 3rem 2rem;
+//   max-width: 1000px;
+//   margin: auto;
+//   text-align: center;
+// }
+
+// .quiz-buttons {
+//   display: flex;
+//   justify-content: space-around;
+//   flex-wrap: wrap;
+//   gap: 1.5rem;
+//   margin-top: 2rem;
+// }
+
+// .quiz-buttons a button {
+//   padding: 1rem 2rem;
+//   min-width: 200px;
+//   font-size: 1.1rem;
+//   background-color: var(--palevioletred);
+//   border-radius: 10px;
+//   border: none;
+//   color: white;
+//   cursor: pointer;
+//   transition: background-color 0.3s ease, transform 0.2s;
+//   box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+// }
+
+// .quiz-buttons a button:hover {
+//   background-color: var(--darkred);
+//   transform: translateY(-3px);
+// }
+
+// .quiz-buttons a {
+//   text-decoration: none;
+// }
